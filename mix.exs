b739a561dc016a23fa6de7eaa13154ef14c88fc3
Guillermo Iguaran <guilleiguaran@gmail.com>
2015-02-13 18:16:31 -0500

    Add mix config

---
+++ mix.exs
@@ -0,0 +1,33 @@
+defmodule ExrmHeroku.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :"exrm-heroku",
+     version: "0.1.0",
+     elixir: "~> 1.0-dev",
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  def application do
+    [applications: []]
+  end
+
+  defp description do
+    """
+    Publish Elixir releases created with exrm release manager to Heroku.
+    """
+  end
+
+  defp deps do
+    [{:exrm, "~> 0.15.0"}]
+  end
+
+  defp package do
+    [ files: ["lib", "priv", "mix.exs", "README.md", "LICENSE"],
+      contributors: ["Guillermo Iguaran"],
+      licenses: ["MIT"],
+      links: [ { "GitHub", "https://github.com/ride/exrm-heroku" } ] ]
+  end
+end
